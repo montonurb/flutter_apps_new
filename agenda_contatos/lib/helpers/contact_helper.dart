86ec44e1cdead8bac17e4b1d4454b8b30a9e3259
montonurb <brunomoncar007@gmail.com>
2022-01-24 22:52:30 -0300

    criando banco de dados e criando função para salvar e buscar os dados

--- agenda_contatos/lib/helpers/contact_helper.dart
+++ agenda_contatos/lib/helpers/contact_helper.dart
@@ -1,14 +1,64 @@
 // ignore_for_file: prefer_const_declarations, unused_import
-
+import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
+final String contactTable = "contactTable";
 final String idColumn = "idColumn";
 final String nameColumn = "nameColumn";
 final String emailColumn = "emailColumn";
 final String phoneColumn = "phoneColumn";
 final String imgColumn = "imgColumn";
 
-class ContactHelper {}
+class ContactHelper {
+  static final ContactHelper _instace = ContactHelper.internal();
+  factory ContactHelper() => _instace;
+  ContactHelper.internal();
+  Database? _db;
+  Future<Database> get db async {
+    if (_db != null) {
+      return _db!;
+    } else {
+      _db = await initDb();
+      return _db!;
+    }
+  }
+
+  Future<Database> initDb() async {
+    final dataBasePath = await getDatabasesPath();
+    final path = join(dataBasePath, "contacts.db");
+
+    return await openDatabase(path, version: 1,
+        onCreate: (Database db, int newerVersion) async {
+      await db.execute(
+        "CREATE TABLE $contactTable("
+        "$idColumn INTEGER PRIMARY KEY, "
+        "$nameColumn TEXT, "
+        "$emailColumn TEXT, "
+        "$phoneColumn TEXT, "
+        "$imgColumn TEXT)",
+      );
+    });
+  }
+
+  Future<Contact> saveContatc(Contact contact) async {
+    Database dbContact = await db;
+    contact.id = await dbContact.insert(contactTable, contact.toMap());
+    return contact;
+  }
+
+  Future<Contact?> getContact(int id) async {
+    Database dbContact = await db;
+    List<Map> maps = await dbContact.query(contactTable,
+        columns: [idColumn, nameColumn, emailColumn, phoneColumn, imgColumn],
+        where: "$idColumn = ?",
+        whereArgs: [id]);
+    if (maps.isNotEmpty) {
+      return Contact.fromMap(maps.first);
+    } else {
+      return null;
+    }
+  }
+}
 
 class Contact {
   int? id;
@@ -25,7 +75,7 @@ class Contact {
     this.img,
   });
 
-  Contact.fromJson(Map<String, dynamic> map) {
+  Contact.fromMap(Map map) {
     id = map[idColumn];
     name = map[nameColumn];
     email = map[emailColumn];
